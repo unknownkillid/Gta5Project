4d4178302e432f64173b2d956a5b285e061ea19e
[R*] Dante <maisuradze600@gmai.com>
2023-01-05 22:11:35 +0400

    royal register/login forms finished

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 let registerButton = document.getElementById('register-page');
 let backgroundMove = document.getElementById('btn-background');
 let loginButton = document.getElementById('login-page');
-let position = backgroundMove.style.left = '615px';
+let position = backgroundMove.style.left = '0px';
+let bodyContainer = document.getElementById('body-container');
+let registerContainer = document.getElementById('register-container');
 
 function backgroundMoveAnimation(){
     if (registerButton.focus){
-        backgroundMove.style.left = '770px';
+        backgroundMove.style.left = '156px';
+        backgroundMove.style.background = '#ff0086';
     }
 }
 
 function backgroundMoveBack(){
     if (loginButton.focus) {
-        backgroundMove.style.left = '615px';
+        backgroundMove.style.left = '0px';
+        backgroundMove.style.background = '#ff00b7';
+    }
+}
+
+function bodyContainerDisplay(){
+    if (registerContainer.style.display = 'none'){
+        bodyContainer.style.display = 'flex'
+    }
+}
+
+function registerContainerDisplay(){
+    if (bodyContainer.style.display = 'none'){
+        registerContainer.style.display = 'flex';
     }
 }
